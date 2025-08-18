52c7fbcb79aefc5f09b485bd780d58ab1222b1be
neuvision-philani <philani.mabuza@neuvision.africa>
2025-08-18 22:32:47 +0200

    Misc Updates

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/** IMPORTS SECTION
+ * **************************************** */
+import * as FunctionsAny from './any/_any.functions.js';
+import * as FunctionsArray from './array/_array.functions.js';
+import * as FunctionsBoolean from './boolean/_boolean.functions.js';
+import * as FunctionsDate from './date/_date.functions.js';
+import * as FunctionsJSON from './json/_json.functions.js';
+import * as FunctionsNumber from './number/_number.functions.js';
+import * as FunctionsText from './text/_text.functions.js';
+import * as FunctionsUniversal from './universal/_universal.functions.js';
+
+/** EXPORT SECTION
+ * **************************************** */
+export {
+  // IMPORTED FUNCTIONS
+  FunctionsAny as any,
+  FunctionsArray as array,
+  FunctionsBoolean as boolean,
+  FunctionsDate as date,
+  FunctionsJSON as json,
+  FunctionsJSON as object,
+  FunctionsNumber as number,
+  FunctionsText as text,
+  FunctionsText as string,
+  FunctionsUniversal as universal,
+}
